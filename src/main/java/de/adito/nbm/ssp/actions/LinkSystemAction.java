f8fe3b81aea6e61a91d579b2d9f9bd17cb24e423
m.kaspera <m.kaspera@adito.de>
2021-04-07 15:27:01 +0200

    Feat: The LinkSystemAction now also tries to set the serverAddress, refactored methods to be less convoluted

--- src/main/java/de/adito/nbm/ssp/actions/LinkSystemAction.java
+++ src/main/java/de/adito/nbm/ssp/actions/LinkSystemAction.java
@@ -4,7 +4,7 @@ import de.adito.nbm.runconfig.api.ISystemInfo;
 import de.adito.nbm.ssp.auth.UserCredentialsManager;
 import de.adito.nbm.ssp.checkout.*;
 import de.adito.nbm.ssp.checkout.clist.CListObject;
-import de.adito.nbm.ssp.facade.ISSPFacade;
+import de.adito.nbm.ssp.facade.*;
 import org.netbeans.api.progress.*;
 import org.openide.*;
 import org.openide.awt.*;
@@ -29,6 +29,9 @@ import java.util.Optional;
 @ActionReference(path = "de/adito/aod/action/system", position = 526)
 public class LinkSystemAction extends NodeAction implements IContextMenuAction
 {
+
+  private static final String PROGRESS_BUNDLE_KEY = "TXT.LinkSystemAction.progress";
+
   @Override
   protected void performAction(Node[] activatedNodes)
   {
@@ -57,51 +60,65 @@ public class LinkSystemAction extends NodeAction implements IContextMenuAction
       dialog.pack();
       dialog.setVisible(true);
 
-      _performLink(systemInfo, panel, loadConfigsCB, dialogDescriptor);
+      CListObject selectedObject = panel.getSelected();
+      if (dialogDescriptor.getValue() == DialogDescriptor.OK_OPTION && selectedObject != null)
+      {
+        _performLink(systemInfo, selectedObject.getSystemDetails(), loadConfigsCB.isSelected());
+      }
     }
   }
 
-  private void _performLink(ISystemInfo systemInfo, SSPCheckoutProjectVisualPanel1 panel, JCheckBox loadConfigsCB, DialogDescriptor dialogDescriptor)
+  /**
+   * @param pSystemInfo    SystemInfo representing the SystemDataModel
+   * @param pSystemDetails SystemDetails for the Cloud system that should be loaded
+   * @param pIsLoadConfigs true if the config files should be loaded and written, false otherwise
+   */
+  private void _performLink(ISystemInfo pSystemInfo, ISSPSystemDetails pSystemDetails, boolean pIsLoadConfigs)
   {
-    Object pressedButtonObject = dialogDescriptor.getValue();
-    if (pressedButtonObject == DialogDescriptor.OK_OPTION)
-    {
-      CListObject selectedObject = panel.getSelected();
-      if (selectedObject != null)
+    // Already create the progressHandle to pass it to the SSPCheckoutExecutor during the task
+    ProgressHandle progressHandle = ProgressHandle.createHandle(NbBundle.getMessage(LinkSystemAction.class, PROGRESS_BUNDLE_KEY));
+    BaseProgressUtils.showProgressDialogAndRun(() -> {
+      String result = pSystemDetails.getSystemdId();
+      File projectDir = FileUtil.toFile(pSystemInfo.getProject().getProjectDirectory());
+      // only do this if the user wants to also get the config files
+      if (pIsLoadConfigs && _handleConfigFiles(pSystemDetails, progressHandle, projectDir))
       {
-        // Already create the progressHandle to pass it to the SSPCheckoutExecutor during the task
-        ProgressHandle progressHandle = ProgressHandle.createHandle(NbBundle.getMessage(LinkSystemAction.class, "TXT.LinkSystemAction.progress"));
-        BaseProgressUtils.showProgressDialogAndRun(() -> {
-          String result = selectedObject.getSystemDetails().getSystemdId();
-          // only do this if the user wants to also get the config files
-          if (loadConfigsCB.isSelected())
-          {
-            File projectDir = FileUtil.toFile(systemInfo.getProject().getProjectDirectory());
-            Object pressedButton = null;
-            // If any of the config files already exist, ask the user if they should be overridden. If no, stop the link
-            if (new File(projectDir, SSPCheckoutExecutor.DEFAULT_SERVER_CONFIG_PATH).exists() ||
-                new File(projectDir, SSPCheckoutExecutor.DEFAULT_TUNNEL_CONFIG_PATH).exists())
-            {
-              pressedButton = DialogDisplayer.getDefault()
-                  .notify(new NotifyDescriptor.Confirmation(NbBundle.getMessage(LinkSystemAction.class, "TXT.LinkSystemAction.overrideQuestion")));
-            }
-            if (!NotifyDescriptor.YES_OPTION.equals(pressedButton))
-            {
-              NotificationDisplayer.getDefault().notify(NbBundle.getMessage(LinkSystemAction.class, "TXT.LinkSystemAction.progress"),
-                                                        NotificationDisplayer.Priority.NORMAL.getIcon(),
-                                                        NbBundle.getMessage(LinkSystemAction.class, "TXT.LinkSystemAction.aborted"), null);
-              return;
-            }
-            SSPCheckoutExecutor.storeConfigs(progressHandle, selectedObject.getSystemDetails(), projectDir,
-                                             ISSPFacade.getInstance(), UserCredentialsManager.getCredentials());
-          }
-          systemInfo.setCloudId(result);
-        }, progressHandle, true);
-        NotificationDisplayer.getDefault().notify(NbBundle.getMessage(LinkSystemAction.class, "TXT.LinkSystemAction.progress"),
-                                                  NotificationDisplayer.Priority.NORMAL.getIcon(),
-                                                  NbBundle.getMessage(LinkSystemAction.class, "TXT.LinkSystemAction.success"), null);
+        return;
       }
+      pSystemInfo.setCloudId(result);
+      NbPreferences.forModule(ISystemInfo.class).put("serverAddressDefault." + projectDir.getPath().replace("\\", "/"), pSystemDetails.getUrl());
+    }, progressHandle, true);
+    NotificationDisplayer.getDefault().notify(NbBundle.getMessage(LinkSystemAction.class, PROGRESS_BUNDLE_KEY),
+                                              NotificationDisplayer.Priority.NORMAL.getIcon(),
+                                              NbBundle.getMessage(LinkSystemAction.class, "TXT.LinkSystemAction.success"), null);
+  }
+
+  /**
+   * @param pSelectedSystem System that should be linked
+   * @param pProgressHandle ProgressHandle to set the current task
+   * @param pProjectDir     ProjectDirectory
+   * @return true if the link should be cancelled, false if the link should be finished
+   */
+  private boolean _handleConfigFiles(ISSPSystemDetails pSelectedSystem, ProgressHandle pProgressHandle, File pProjectDir)
+  {
+    Object pressedButton = null;
+    // If any of the config files already exist, ask the user if they should be overridden. If no, stop the link
+    if (new File(pProjectDir, SSPCheckoutExecutor.DEFAULT_SERVER_CONFIG_PATH).exists() ||
+        new File(pProjectDir, SSPCheckoutExecutor.DEFAULT_TUNNEL_CONFIG_PATH).exists())
+    {
+      pressedButton = DialogDisplayer.getDefault()
+          .notify(new NotifyDescriptor.Confirmation(NbBundle.getMessage(LinkSystemAction.class, "TXT.LinkSystemAction.overrideQuestion")));
     }
+    if (!NotifyDescriptor.YES_OPTION.equals(pressedButton))
+    {
+      NotificationDisplayer.getDefault().notify(NbBundle.getMessage(LinkSystemAction.class, PROGRESS_BUNDLE_KEY),
+                                                NotificationDisplayer.Priority.NORMAL.getIcon(),
+                                                NbBundle.getMessage(LinkSystemAction.class, "TXT.LinkSystemAction.aborted"), null);
+      return true;
+    }
+    SSPCheckoutExecutor.storeConfigs(pProgressHandle, pSelectedSystem, pProjectDir,
+                                     ISSPFacade.getInstance(), UserCredentialsManager.getCredentials());
+    return false;
   }
 
   @Override
