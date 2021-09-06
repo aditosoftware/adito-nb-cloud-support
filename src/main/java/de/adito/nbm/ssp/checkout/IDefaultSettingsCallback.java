78034d17367f4e4edcc2a17d98f9b2cb33fccfbc
Werner Glanzer <w.glanzer@adito.de>
2021-09-06 11:48:42 +0200

    fix: changed encoding of "IDefaultSettingsCallback" to windows-1252

--- src/main/java/de/adito/nbm/ssp/checkout/IDefaultSettingsCallback.java
+++ src/main/java/de/adito/nbm/ssp/checkout/IDefaultSettingsCallback.java
@@ -5,8 +5,8 @@ import org.jetbrains.annotations.*;
 import java.awt.*;
 
 /**
- * Gibt die Standardwerte für URLs, Projektpfad und Datenbankpfad an
- * Für die URLs wird eine eigene Klasse verwendet, die URL und Zugangsdaten verbindet
+ * Gibt die Standardwerte f?r URLs, Projektpfad und Datenbankpfad an
+ * F?r die URLs wird eine eigene Klasse verwendet, die URL und Zugangsdaten verbindet
  *
  * @author s.danner, 19.09.13
  */
@@ -14,19 +14,19 @@ public interface IDefaultSettingsCallback
 {
 
   /**
-   * Gibt einen Default-Pfad zurück, wo die Projekt-Dateien gespeichert werden sollen
+   * Gibt einen Default-Pfad zur?ck, wo die Projekt-Dateien gespeichert werden sollen
    */
   @Nullable
   String getDefaultProjectPath();
 
   /**
-   * Gibt die Scrollgeschwindigkeit für die Detailseite zurück
+   * Gibt die Scrollgeschwindigkeit f?r die Detailseite zur?ck
    * Die Angabe erfolgt als positive Zahl ("Unit"-Increment)
    */
   int getDefaultScrollSpeed();
 
   /**
-   * Gibt die Default-Hintergrundfarbe für ein VisualPanel des Wizards zurück
+   * Gibt die Default-Hintergrundfarbe f?r ein VisualPanel des Wizards zur?ck
    */
   @NotNull
   Color getDefaultBackgroundColor();
