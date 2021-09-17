b59cd194f2d735a73f1724a06f06d16ada7bc991
m.kaspera <m.kaspera@adito.de>
2021-09-17 14:50:46 +0200

    Feat: The start and stop actions now determine their status asynchronously

--- src/main/java/de/adito/nbm/ssp/actions/StartSystemAction.java
+++ src/main/java/de/adito/nbm/ssp/actions/StartSystemAction.java
@@ -1,13 +1,14 @@
 package de.adito.nbm.ssp.actions;
 
 import com.auth0.jwt.interfaces.DecodedJWT;
+import de.adito.actions.AbstractAsyncNodeAction;
 import de.adito.nbm.runconfig.api.ISystemInfo;
 import de.adito.nbm.ssp.auth.UserCredentialsManager;
 import de.adito.nbm.ssp.facade.ISSPFacade;
+import org.jetbrains.annotations.NotNull;
 import org.openide.awt.*;
 import org.openide.nodes.Node;
 import org.openide.util.*;
-import org.openide.util.actions.NodeAction;
 
 /**
  * @author m.kaspera, 21.10.2020
@@ -15,7 +16,7 @@ import org.openide.util.actions.NodeAction;
 @ActionID(category = "adito/aods", id = "de.adito.nbm.ssp.actions.StartSystemAction")
 @ActionRegistration(displayName = "")
 @ActionReference(path = "de/adito/aod/action/system", position = 524)
-public class StartSystemAction extends NodeAction implements IContextMenuAction, IStartSystemAction
+public class StartSystemAction extends AbstractAsyncNodeAction implements IContextMenuAction, IStartSystemAction
 {
   @Override
   protected void performAction(Node[] activatedNodes)
@@ -25,7 +26,7 @@ public class StartSystemAction extends NodeAction implements IContextMenuAction,
   }
 
   @Override
-  protected boolean enable(Node[] activatedNodes)
+  protected boolean enable0(@NotNull Node[] activatedNodes)
   {
     ISystemInfo systemInfo = getSystemInfoFromNodes(activatedNodes);
     if (systemInfo != null)
@@ -40,7 +41,7 @@ public class StartSystemAction extends NodeAction implements IContextMenuAction,
         return !sspFacade.isSystemRunning(jwt.getSubject(), jwt, cloudId);
       }
     }
-    return true;
+    return false;
   }
 
   @Override
